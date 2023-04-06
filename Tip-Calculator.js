ade5fb8f23d454a660ce9751eff8acafd9b125f6
svang01 <shattv@yahoo.com>
2023-04-05 23:40:15 -0400

    tip

--- Tip-Calculator.js
+++ Tip-Calculator.js
@@ -1,11 +1,12 @@
 let bill = 200.31;
 
 let calculateTip = function(bill) {
-  return bill * 0.2;
+  return Math.round((bill *0.2) * 100) / 100;
 }
 
 let getBillTotal = function(bill) {
   return Math.round((bill + calculateTip(bill)) * 100) / 100;
 }
 
+console.log(calculateTip(bill));
 console.log(getBillTotal(bill));
