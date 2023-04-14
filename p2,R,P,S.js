0aadeba8a0b5a82cd890fe7e16dc51114ba497b1
svang01 <shattv@yahoo.com>
2023-04-13 20:38:53 -0400

    error

--- p2,R,P,S.js
+++ p2,R,P,S.js
@@ -52,59 +52,7 @@ function playRound(player1, player2) {
   }
 
   return winner;
-}
-
-// // Play a single game between player1 and player2
-// const gameWinner = playGame(player1, player2, 5);
-// console.log(gameWinner.name + " wins the game!");
-
-// // Define the playGame function
-// function playGame(player1, player2, playUntil) {
-//   playUntil = playUntil || 3;
-
-//   while (player1.wins < playUntil && player2.wins < playUntil) {
-//     playRound(player1, player2);
-//   }
-
-//   if (player1.wins >= playUntil) {
-//     return player1;
-//   } else {
-//     return player2;
-//   }
-// }
-
-// // Define the playGame function
-// function playGame(player1, player2, playUntil) {
-//     playUntil = playUntil || 3;
-  
-//     while (player1.wins < playUntil && player2.wins < playUntil) {
-//       playRound(player1, player2);
-//     }
-  
-//     if (player1.wins >= playUntil) {
-//       return player1;
-//     } else {
-//       return player2;
-//     }
-// }
-  
-// // Define the playTournament function
-// function playTournament(player1, player2, player3, player4, playUntil) {
-//     playUntil = playUntil || 5;
-  
-//     console.log("Semi-final 1: " + player1.name + " vs " + player2.name);
-//     const semiFinalWinner1 = playGame(player1, player2, playUntil);
-//     console.log(semiFinalWinner1.name + " advances to the final round!");
-  
-//     console.log("Semi-final 2: " + player3.name + " vs " + player4.name);
-//     const semiFinalWinner2 = playGame(player3, player4, playUntil);
-//     console.log(semiFinalWinner2.name + " advances to the final round!");
-  
-//     console.log("Final: " + semiFinalWinner1.name + " vs " + semiFinalWinner2.name);
-//     const finalWinner = playGame(semiFinalWinner1, semiFinalWinner2, playUntil);
-//     console.log(finalWinner.name + " is the World Champion!");}
-
-//     playTournament(player1, player2, player3, player4, 5);
+};
 
 function playGame(playerOne, playerTwo, playUntil) {
   let p1W = 0;
