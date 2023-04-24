ed438c13ad6219e9cb72355e4aa24d8b87dbfc06
svang01 <shattv@yahoo.com>
2023-04-24 13:36:58 -0400

    es62

--- JS 4-24/script.js
+++ JS 4-24/script.js
@@ -13,6 +13,7 @@ console.log(square(5));
 // Use the map method to create a new array called squares that contains the squares of the numbers in the numbers array.
 let squares = arrayMerge.map(square);
 console.log(arrayMerge.map(square));
+
 // Define an arrow function called isEven that accepts a number as an argument and returns true if the number is even, otherwise false.
 const isEven = (num) => num % 2 === 0;
 console.log(isEven(4));
@@ -20,10 +21,12 @@ console.log(isEven(4));
 // Use the filter method to create a new array called evenSquares that contains only the even squares from the squares array.
 const evenSquares = squares.filter(isEven)
 console.log(evenSquares);
+
 // Use destructuring assignment to extract the first and second elements of the evenSquares array into constants firstEvenSquare and secondEvenSquare.
 const [firstEvenSquare, secondEvenSquare] = evenSquares
 console.log(firstEvenSquare);
 console.log(secondEvenSquare);
+
 // Use a template literal to log the original numbers array, the squares array, the evenSquares array, and the extracted even squares.
 console.log(`Original Array: ${arrayMerge}`);
 console.log(`Square Array: ${squares}`);
