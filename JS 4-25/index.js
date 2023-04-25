65ca3bee027db736457140bf6640039d7681b9a2
svang01 <shattv@yahoo.com>
2023-04-25 13:30:13 -0400

    initial commit

---
+++ JS 4-25/index.js
@@ -0,0 +1,5 @@
+// Create an index.js file and update package.json so that npm start will run it
+console.log("hello");
+
+// Require or import the 'moment' modules in your application.
+const moment = require('moment');
